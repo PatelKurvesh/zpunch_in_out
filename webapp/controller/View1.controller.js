42759bec7d73fcac9d166dddd8ca0e18a2f1c2dc
Kurvesh Patel <patelkurvesh8866@gmail.com>
2025-01-06 08:52:39 +0000

    punch in barcode added

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,10 +1,32 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "sap/ndc/BarcodeScanner"
+], (Controller, BarcodeScanner) => {
     "use strict";
 
     return Controller.extend("zpunchinout.controller.View1", {
         onInit() {
+        },
+
+        onPunchInButtonPress : function(){
+            BarcodeScanner.scan(
+                function (result) {
+                    if (result.cancelled) {
+                        sap.m.MessageToast.show("Scanning cancelled.");
+                    } else {
+                        sap.m.MessageToast.show("Scanned Barcode: " + result.text);
+                        // Handle the scanned result (e.g., send it to the backend or display it in the UI)
+                        console.log("Scanned Result:", result.text);
+                    }
+                },
+                function (error) {
+                    sap.m.MessageToast.show("Scanning failed: " + error);
+                    console.error("Barcode Scanning Error:", error);
+                }
+            );
+        },
+        onPunchOutButtonPress:function(){
+
         }
     });
 });
---
+++ webapp/controller/View2.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(
+	Controller
+) {
+	"use strict";
+
+	return Controller.extend("zpunchinout.controller.View2", {
+        /**
+         * @override
+         */
+        onInit: function() {
+            
+            
+        
+        }
+	});
+});
